02356ae595b8551cc47056272dd8eb5523a22203
sdeagh <huw.burton@btinternet.com>
2018-02-12 17:15:17 +0000

    background image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import Title from './components/Title';
+import Search from './components/Search';
+import MovieList from './components/MovieList';
 
 class App extends Component {
 
-  constructor() {
-    super()
-    this.state = {
-      searchterm: "",
-      movies: "",
-      singlemovie: ""
-    }
-  }
+	constructor() {
+		super()
+		this.state = {
+			searchterm: "",
+			movies: "",
+			singlemovie: ""
+		}
+	}
 
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+	render() {
+    	return (
+				
+				<div class='outer-scratch'>
+  					<div class="inner-scratch">
+    					<div class="background grain">
+
+							<h1>Hellloooo</h1>
+						</div>
+  					</div>
+				</div>
+
+    	);
+  	}
 }
 
 export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Title.css';
+
+const Title = () => {
+    return (
+        <div className="d-flex flex-row justify-content-center">
+            <h1>Movie Database</h1>
+        </div>
+    )
+}
+
+export default Title;
